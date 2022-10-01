582c603dd0c587f4fb60c85c944c89673045a611
nadinejuraschek <nadinejwebdev@gmail.com>
2022-10-01 12:25:02 +0200

    New: removed react moment

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
 # Megaphone
+
 A Twitter-like full stack application. Built with React, Redux, and Node.js.
 
 ### Live Demo
 
 ### Technologies
-* CSS Flexbox & Grid
-* React
-* Redux
-* Node.js
-* Express.js
-* React Moment
-* bcrypt
-* jsonwebtoken
-* MongoDB
-* mongoose
-* axios
+
+- CSS Flexbox & Grid
+- React
+- Redux
+- Node.js
+- Express.js
+- Dayjs
+- bcrypt
+- jsonwebtoken
+- MongoDB
+- mongoose
+- axios
+- TypeScript
 
 ### Images
+
 [Unsplash](https://unsplash.com/)
 
 ### Icons
+
 [Feather](https://feathericons.com/)
--- frontend/src/components/MessageItem/index.tsx
+++ frontend/src/components/MessageItem/index.tsx
@@ -1,7 +1,7 @@
 import { IMessageItem } from './types';
 import Icon from '../Icon';
-import Moment from 'react-moment';
 import Profile from '../Profile';
+import dayjs from 'dayjs';
 import deleteIcon from '../../icons/delete.svg';
 import styles from './message.module.css';
 
@@ -12,33 +12,35 @@ const MessageItem = ({
   removeMessage,
   text,
   username,
-}: IMessageItem): JSX.Element => (
-  <li className={styles.message}>
-    <div className={styles.header}>
-      <div className={styles.userinfo}>
-        <Profile
-          profileImageUrl={profileImageUrl}
-          username={username}
-          height='5rem'
-          width='5rem'
-        />
-        <div className={styles.name}>@{username} &nbsp;</div>
-      </div>
-      {isCorrectUser && (
-        <div className={styles.delete} onClick={removeMessage}>
-          <Icon icon={deleteIcon} iconName='Delete Message' />
+}: IMessageItem): JSX.Element => {
+  const printDate = dayjs(date).format('h:mm a, DD MMM YYYY');
+
+  return (
+    <li className={styles.message}>
+      <div className={styles.header}>
+        <div className={styles.userinfo}>
+          <Profile
+            profileImageUrl={profileImageUrl}
+            username={username}
+            height='5rem'
+            width='5rem'
+          />
+          <div className={styles.name}>@{username} &nbsp;</div>
         </div>
-      )}
-    </div>
-    <div className={styles.body}>
-      <p>{text}</p>
-    </div>
-    <div className={styles.footer}>
-      <p className={styles.date}>
-        <Moment format='h:mm a, DD MMM YYYY'>{date}</Moment>
-      </p>
-    </div>
-  </li>
-);
+        {isCorrectUser && (
+          <div className={styles.delete} onClick={removeMessage}>
+            <Icon icon={deleteIcon} iconName='Delete Message' />
+          </div>
+        )}
+      </div>
+      <div className={styles.body}>
+        <p>{text}</p>
+      </div>
+      <div className={styles.footer}>
+        <p className={styles.date}>{printDate}</p>
+      </div>
+    </li>
+  );
+};
 
 export default MessageItem;
